4a2e0c3ab251339f66d64ffd0d96aa0884b42bfe
neon-lab-dev <162788012+neon-lab-dev@users.noreply.github.com>
2025-01-07 16:08:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["*", "http://localhost:5173"],
+  origin: ["*", "http://localhost:5173","https://forge-admin-eight.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "DELETE", "PUT"],
 }));
