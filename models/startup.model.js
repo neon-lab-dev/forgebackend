c842324d6be6976692a239fae966a638175fea77
neon-lab-dev <162788012+neon-lab-dev@users.noreply.github.com>
2025-01-21 10:23:24 +0530

    Update startup.model.js

--- models/startup.model.js
+++ models/startup.model.js
@@ -5,24 +5,19 @@ const { Schema, model } = mongoose;
 const StartupSchema = new Schema({
   name: {
     type: String,
-    required: [true, "Name is required"],
   },
   logo: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Image", // Reference the Image model
-    required: [true, "Logo is required"],
   },
   websiteUrl: {
     type: String,
-    required: [true, "Website URL is required"],
   },
   hardwareTech: {
     type: String,
-    required: [true, "Hardware Tech is required"],
   },
   hardwareInnovations: {
     type: String,
-    required: [true, "Hardware Innovations is required"],
   },
   keyInvestors: [{
     type: mongoose.Schema.Types.ObjectId,
@@ -34,7 +29,6 @@ const StartupSchema = new Schema({
   }],
   about: {
     type: String,
-    required: [true, "About is required"],
   },
   metricFeatures: [{
     heading: {
