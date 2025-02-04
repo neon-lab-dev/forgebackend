4cd71cb71a13cc6605753ed9b6f7fc169a7b9fb7
Sandip Sapkota <079bct076@ioepc.edu.np>
2025-02-04 19:47:03 +0545

    feat: add getProgramsAndGrants endpoint to retrieve distinct programmes and grants

--- routes/startup.routes.js
+++ routes/startup.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createStartup, getStartups, getSingleStartup, editStartup, deleteStartup, makeInactive, makeActive } from "../controllers/startup.controller.js";
+import { createStartup, getStartups, getSingleStartup, editStartup, deleteStartup, makeInactive, makeActive, getProgramsAndGrants } from "../controllers/startup.controller.js";
 import { authenticate } from "../middlewares/auth.js";
 
 const router = Router();
@@ -8,9 +8,10 @@ const router = Router();
 
 router.post("/create", authenticate(), createStartup);
 router.get("/", getStartups);
-router.get("/:id", getSingleStartup).put("/:id", authenticate(), editStartup).delete("/:id", authenticate(), deleteStartup);
 router.put("/change-inactive/:id", authenticate(), makeInactive);
 router.put("/change-active/:id", authenticate(), makeActive);
+router.get("/programs-grants", getProgramsAndGrants);
+router.get("/:id", getSingleStartup).put("/:id", authenticate(), editStartup).delete("/:id", authenticate(), deleteStartup);
 
 
 export default router;
